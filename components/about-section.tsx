842427123b9396db6d53c976e4718703d747ab05
leonislam-dev <ashrafulislamleon92@gmail.com>
2025-07-05 01:03:26 +0600

    update

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -25,7 +25,7 @@ const skills = [
   {
     icon: Users,
     title: "Leadership",
-    description: "Experienced in leading development teams and mentoring junior developers.",
+    description: "Experienced in leading development teams and mentoring.",
   },
 ]
 
