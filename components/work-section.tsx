f6f87c78dcd3180e385222a08af870260080a461
leonislam-dev <ashrafulislamleon92@gmail.com>
2025-07-24 23:40:02 +0600

    update

--- components/work-section.tsx
+++ components/work-section.tsx
@@ -97,7 +97,7 @@ export function WorkSection() {
                   <CardContent
                     className={`p-6 ${project.featured ? "md:w-1/2 md:flex md:flex-col md:justify-center" : ""}`}
                   >
-                    <div className="flex flex-wrap gap-2 mb-4">
+                    <div className="flex flex-wrap gap-2">
                       {project.tags.map((tag) => (
                         <span
                           key={tag}
