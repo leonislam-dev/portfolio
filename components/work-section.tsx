1a9509f67001b1747fa7c23b239bd215884902f3
leonislam-dev <ashrafulislamleon92@gmail.com>
2025-08-10 21:24:36 +0600

    update

--- components/work-section.tsx
+++ components/work-section.tsx
@@ -91,7 +91,7 @@ export function WorkSection() {
                       height={400}
                       className="w-full h-64 md:h-70 object-cover transition-transform duration-500 group-hover:scale-110"
                     />
-                    <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-80 transition-opacity duration-400" />
+                    <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-80 transition-opacity duration-300" />
                   </div>
 
                   <CardContent
